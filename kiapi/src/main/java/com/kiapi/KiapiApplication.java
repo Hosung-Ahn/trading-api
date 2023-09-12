e0d95d65c0ec7aaca8c8a5674c03939cbbb7a0c0
Hosung <an3735297@pusan.ac.kr>
2023-09-12 10:28:53 +0900

    프로젝트 생성

---
+++ kiapi/src/main/java/com/kiapi/KiapiApplication.java
@@ -0,0 +1,13 @@
+package com.kiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiapiApplication.class, args);
+	}
+
+}
---
+++ kiapi/src/test/java/com/kiapi/KiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
