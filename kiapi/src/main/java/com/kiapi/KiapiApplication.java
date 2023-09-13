84d24fd991a668a6885fc2b01e6eb19237f5c73c
Hosung <an3735297@naver.com>
2023-09-13 14:34:32 +0900

    feat : login : init role
    role 은 정해진 개수만 db에 포홤되고 이후 변동사항이 없습니다.
    앱이 시작될 때도 role 은 미리 db에 들어가 있습니다.

--- kiapi/src/main/java/com/kiapi/KiapiApplication.java
+++ kiapi/src/main/java/com/kiapi/KiapiApplication.java
@@ -1,13 +1,34 @@
 package com.kiapi;
 
+import com.kiapi.entity.member.ERole;
+import com.kiapi.entity.member.Role;
+import com.kiapi.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KiapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KiapiApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner initRoles(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName(ERole.ROLE_USER) == null) {
+				roleRepository.save(new Role(ERole.ROLE_USER));
+			}
+			if (roleRepository.findByName(ERole.ROLE_MODERATOR) == null) {
+				roleRepository.save(new Role(ERole.ROLE_MODERATOR));
+			}
+			if (roleRepository.findByName(ERole.ROLE_ADMIN) == null) {
+				roleRepository.save(new Role(ERole.ROLE_ADMIN));
+			}
+		};
 	}
 
 }
